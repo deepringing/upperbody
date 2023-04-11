7b38bfc6caeb10b66f3a1705c1c8b2fc0303b5b5
YuLim2 <jyulim0120@gmail.com>
2023-04-11 13:42:39 +0900

    ADD: Splash screean and sing up screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      title: 'upper_singup',
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Color(0xffFFFFFF),
+        appBar: buildAppBar(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "입력한 정보가 맞다면\n아래 확인 버튼을 눌러주세요.",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            buildTextFeild("이메일"),
+            buildTextFeild("닉네임"),
+            buildTextFeild("비밀번호"),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 50),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (_) => Board()));
+                },
+                // ignore: sort_child_properties_last
+                child: const Text(
+                  "로그인",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color(0xffFFFFFFF),
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xff7591F7),
+                  minimumSize: const Size(double.infinity, 52),
+                  shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                          width: 1.0, color: Color(0xffD6A7D7)),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Column buildTextFeild(String s) {
+    return Column(children: [
+      Row(children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(30, 20, 30, 5),
+          child: Text(
+            s,
+            style: const TextStyle(
+                fontSize: 15,
+                color: Color(0xff757575),
+                fontWeight: FontWeight.w300),
+          ),
+        ),
+      ]),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          SizedBox(
+            width: 335,
+            height: 50,
+            child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xffFFFFFF),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xff6A6A6A), width: 0.7),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xff6A6A6A), width: 0.7),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                )),
+          ),
+        ],
+      ),
+    ]);
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.white,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 15,
+          ),
+          SizedBox(
+            width: 50,
+            child: Image.asset(
+              'assets/images/logo/logo.png',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
