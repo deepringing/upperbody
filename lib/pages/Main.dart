6876140f66f5b7dc4101bdfeb47d7f2f9aa3308d
parkdaxun <parkde0207@gmail.com>
2023-04-25 13:38:12 +0900

    [ADD] Text

--- lib/pages/Main.dart
+++ lib/pages/Main.dart
@@ -20,9 +20,23 @@ class _MainPageState extends State<MainPage> {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.only(top:67, left:30),
+              child: Text(
+                  "안녕하세요. 가나다은님, \n졸업선배님께 조언을 받아보아요",
+                  style: TextStyle(fontSize: 20), //TODO : fontWeight bold
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:35, left: 30),
+              child: Text(
+                  "진주여자중학교",
+                  style: TextStyle(fontSize: 15), //TODO : fontWeight bold
+              ),
+            ),
           ],
+          ),
         ),
-      ),
     );
   }
 
