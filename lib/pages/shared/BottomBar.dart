6144f3fc23a0f0bc32788f93268101ef5da15a5d
YuLim2 <jyulim0120@gmail.com>
2023-06-19 23:27:39 +0900

    ADD: Main and Detail page

---
+++ lib/pages/shared/BottomBar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BottomBar {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Demo'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+        ],
+      ),
+    );
+  }
+}
